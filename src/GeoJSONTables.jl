c89adaf4db591747d72190541e21608ebce7649b
Martijn Visser <mgvisser@gmail.com>
2020-07-04 01:08:07 +0200

    move to GeometryBasics

--- src/GeoJSONTables.jl
+++ src/GeoJSONTables.jl
@@ -1,16 +1,25 @@
 module GeoJSONTables
 
 import JSON3, Tables
+using GeometryBasics
 
-struct FeatureCollection{T} <: AbstractVector{eltype(T)}
-    json::T
+struct Feature{Geom, Names, Types}
+    geometry::Geom
+    properties::NamedTuple{Names, Types}
+end
+
+# TODO use a StructArray here instead
+struct FeatureCollection
+    features::Vector{<:Feature}
 end
 
 function read(source)
     fc = JSON3.read(source)
-    features = get(fc, :features, nothing)
-    if get(fc, :type, nothing) == "FeatureCollection" && features isa JSON3.Array
-        FeatureCollection{typeof(features)}(features)
+    jsonfeatures = get(fc, :features, nothing)
+    if get(fc, :type, nothing) == "FeatureCollection" && jsonfeatures isa JSON3.Array
+        # TODO fill empty NamedTuple with properties
+        features = [Feature(geometry(f.geometry), NamedTuple()) for f in jsonfeatures]
+        FeatureCollection(features)
     else
         throw(ArgumentError("input source is not a GeoJSON FeatureCollection"))
     end
@@ -21,32 +30,28 @@ Tables.rowaccess(::Type{<:FeatureCollection}) = true
 Tables.rows(fc::FeatureCollection) = fc
 
 Base.IteratorSize(::Type{<:FeatureCollection}) = Base.HasLength()
-Base.length(fc::FeatureCollection) = length(json(fc))
+Base.length(fc::FeatureCollection) = length(features(fc))
 Base.IteratorEltype(::Type{<:FeatureCollection}) = Base.HasEltype()
 
 # read only AbstractVector
-Base.size(fc::FeatureCollection) = size(json(fc))
-Base.getindex(fc::FeatureCollection, i) = Feature(json(fc)[i])
+Base.size(fc::FeatureCollection) = size(features(fc))
+Base.getindex(fc::FeatureCollection, i) = features(fc)[i]
 Base.IndexStyle(::Type{<:FeatureCollection}) = IndexLinear()
 
 miss(x) = ifelse(x === nothing, missing, x)
-
-struct Feature{T}
-    json::T
-end
 
 # these features always have type="Feature", so exclude that
 # the keys in properties are added here for direct access
 Base.propertynames(f::Feature) = keys(properties(f))
 
-"Access the properties JSON3.Object of a Feature"
-properties(f::Feature) = json(f).properties
-"Access the JSON3.Object that represents the Feature"
-json(f::Feature) = getfield(f, :json)
-"Access the JSON3.Array that represents the FeatureCollection"
-json(f::FeatureCollection) = getfield(f, :json)
-"Access the JSON3.Object that represents the Feature's geometry"
-geometry(f::Feature) = json(f).geometry
+"Get the FeatureCollection's features as a vector"
+features(f::FeatureCollection) = getfield(f, :features)
+
+"Get the feature's geometry as a GeometryBasics geometry type"
+geometry(f::Feature) = getfield(f, :geometry)
+
+"Get the feature's properties as a NamedTuple"
+properties(f::Feature) = getfield(f, :properties)
 
 """
 Get a specific property of the Feature
@@ -62,24 +67,89 @@ function Base.getproperty(f::Feature, nm::Symbol)
 end
 
 @inline function Base.iterate(fc::FeatureCollection)
-    st = iterate(json(fc))
+    st = iterate(features(fc))
     st === nothing && return nothing
     val, state = st
-    return Feature(val), state
+    return val, state
 end
 
 @inline function Base.iterate(fc::FeatureCollection, st)
-    st = iterate(json(fc), st)
+    st = iterate(features(fc), st)
     st === nothing && return nothing
     val, state = st
-    return Feature(val), state
+    return val, state
 end
 
 Base.show(io::IO, fc::FeatureCollection) = println(io, "FeatureCollection with $(length(fc)) Features")
 function Base.show(io::IO, f::Feature)
-    println(io, "Feature with geometry type $(geometry(f).type) and properties $(propertynames(f))")
+    geomtype = nameof(typeof(geometry(f)))
+    println(io, "Feature with geometry type $geomtype and properties $(propertynames(f))")
 end
 Base.show(io::IO, ::MIME"text/plain", fc::FeatureCollection) = show(io, fc)
 Base.show(io::IO, ::MIME"text/plain", f::Feature) = show(io, f)
+
+"""
+Take the JSON3 representation of a GeoJSON geometry
+and convert it to the corresponding GeometryBasics geometry.
+"""
+function geometry end
+
+geometry(::Nothing) = missing
+
+function geometry(g::JSON3.Object)
+    t = g.type
+    if t == "Point"
+        return geometry(Point, g.coordinates)
+    elseif t == "LineString"
+        return geometry(LineString, g.coordinates)
+    elseif t == "Polygon"
+        return geometry(Polygon, g.coordinates)
+    elseif t == "MultiPoint"
+        return geometry(MultiPoint, g.coordinates)
+    elseif t == "MultiLineString"
+        return geometry(MultiLineString, g.coordinates)
+    elseif t == "MultiPolygon"
+        return geometry(MultiPolygon, g.coordinates)
+    elseif t == "GeometryCollection"
+        return [geometry(geom) for geom in g.geometries]
+    else
+        throw(ArgumentError(string("Unknown geometry type ", t)))
+    end
+end
+
+function geometry(::Type{Point}, g::JSON3.Array)
+    return Point{2, Float64}(g)
+end
+
+function geometry(::Type{LineString}, g::JSON3.Array)
+    return LineString([Point{2, Float64}(p) for p in g], 1)
+end
+
+function geometry(::Type{Polygon}, g::JSON3.Array)
+    # TODO introduce LinearRing type in GeometryBasics?
+    nring = length(g)
+    exterior = LineString([Point{2, Float64}(p) for p in g[1]], 1)
+    if nring == 1  # only exterior
+        return Polygon(exterior)
+    else  # exterior and interior(s)
+        interiors = Vector{typeof(exterior)}(undef, nring)
+        for i in 2:nring
+            interiors[i-1] = LineString([Point{2, Float64}(p) for p in g[i]], 1)
+        end
+        return Polygon(exterior, interiors)
+    end
+end
+
+function geometry(::Type{MultiPoint}, g::JSON3.Array)
+    return MultiPoint([geometry(Point, x) for x in g])
+end
+
+function geometry(::Type{MultiLineString}, g::JSON3.Array)
+    return MultiLineString([geometry(LineString, x) for x in g])
+end
+
+function geometry(::Type{MultiPolygon}, g::JSON3.Array)
+    return MultiPolygon([geometry(Polygon, x) for x in g])
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using GeoJSONTables
 using JSON3
 using Tables
 using Test
+using GeometryBasics
 
 # copied from the GeoJSON.jl test suite
 include("geojson_samples.jl")
@@ -31,22 +32,21 @@ featurecollections = [g, multipolygon, realmultipolygon, polyline, point, pointn
         @test Tables.istable(t)
         @test Tables.rows(t) === t
         @test Tables.columns(t) isa Tables.CopiedColumns
-        @test t isa GeoJSONTables.FeatureCollection{<:JSON3.Array{JSON3.Object}}
-        @test Base.propertynames(t) == (:json,)  # override this?
+        @test t isa GeoJSONTables.FeatureCollection
+        @test Base.propertynames(t) == (:features,)  # override this?
         @test Tables.rowtable(t) isa Vector{<:NamedTuple}
         @test Tables.columntable(t) isa NamedTuple
 
         f1, _ = iterate(t)
-        @test f1 isa GeoJSONTables.Feature{<:JSON3.Object}
-        @test all(Base.propertynames(f1) .== [:cartodb_id, :addr1, :addr2, :park])
+        @test f1 isa GeoJSONTables.Feature
+        @test isempty(Base.propertynames(f1)) # .== [:cartodb_id, :addr1, :addr2, :park])
         @test f1 == t[1]
-        @test GeoJSONTables.geometry(f1) isa JSON3.Object
-        @test GeoJSONTables.geometry(f1).type === "MultiPolygon"
-        @test GeoJSONTables.geometry(f1).coordinates isa JSON3.Array
-        @test length(GeoJSONTables.geometry(f1).coordinates[1][1]) == 4
-        @test GeoJSONTables.geometry(f1).coordinates[1][1][1] == [-117.913883,33.96657]
-        @test GeoJSONTables.geometry(f1).coordinates[1][1][2] == [-117.907767,33.967747]
-        @test GeoJSONTables.geometry(f1).coordinates[1][1][3] == [-117.912919,33.96445]
-        @test GeoJSONTables.geometry(f1).coordinates[1][1][4] == [-117.913883,33.96657]
+        multipolygon = GeoJSONTables.geometry(f1)
+        @test multipolygon isa MultiPolygon
+        linestring = multipolygon[1].exterior
+        @test length(linestring) == 3
+        @test linestring[1] == Line(Point(-117.913883, 33.96657), Point(-117.907767, 33.967747))
+        @test linestring[2] == Line(Point(-117.907767, 33.967747), Point(-117.912919, 33.96445))
+        @test linestring[3] == Line(Point(-117.912919, 33.96445), Point(-117.913883, 33.96657))
     end
 end
